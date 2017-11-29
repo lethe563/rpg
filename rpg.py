6dc802da6fee644fa37c0fe686d952b0718e9125
lethe563 <lethe563@gmail.com>
2017-11-29 17:21:04 +0000

    Combat has it's own function

--- rpg.py
+++ rpg.py
@@ -1,10 +1,5 @@
-def main():
-    health = 100
-    attack = 10
-    enemy_health = 100
-    enemy_attack = 5
-
-    while health >0:
+def combat(health, enemy_health, attack, enemy_attack):
+    while health > 0 and enemy_health > 0:
         command = input("Do you wish to attack? (y/n)")
         if command == "y":
             enemy_health -= attack
@@ -15,5 +10,15 @@ def main():
             print("Failure! Just press y next time.\nYour health is now: " + str(health) + "\nYour enemy's health is now: " + str(enemy_health))
         else:
             print("Please enter either y or n")
+
+
+
+def main():
+    health = 100
+    enemy_health = 100
+    attack = 10
+    enemy_attack = 5
+
+    combat(health, enemy_health, attack, enemy_attack)
 
 main()
