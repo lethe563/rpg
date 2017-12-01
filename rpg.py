727ab75a16c08e253774959dccb38f89a6fe1ea8
lethe563 <lethe563@gmail.com>
2017-12-01 13:33:37 +0000

    Tidied up the 'continue' code I was having trouble with into a function called 'next_step()'. Found the reason code was clunky was because the code was optimised for testing, and not in the format which it woul be used in the finished product. Now it is more in line with how it will be used in the final product, with much room for expansion. Rooms and enemies are now both generated in the next_step() function. created a death() function, to check if the player is dead.

--- rpg.py
+++ rpg.py
@@ -2,7 +2,7 @@ import json
 import random
 
 def combat(health, attack, enemy_name, enemy_health, enemy_attack):
-    print("Enemy name: " + enemy_name + "\nEnemy health: " + str(enemy_health) + "\nCurrent health : " + str(health))
+    #print("Enemy name: " + enemy_name + "\nEnemy health: " + str(enemy_health) + "\nCurrent health : " + str(health))
     while health > 0 and enemy_health > 0:
 
         command = input("Do you wish to attack? (y/n)d")
@@ -37,35 +37,55 @@ def baddies():
     baddie = baddies_list[random_baddie] #Pulling the dictonary for the enemy, by using the name generated in random_baddie line, as a key.
     return baddie
 
+def next_step(first_time):
+    decision = False #Decision to continue on quest
+    while decision == False: #Continue loop until a decision is reached
+        if first_time == True:
+            quest = input("Would you like to start your quest? y/n")
+        else:
+            quest = input("Would you like to continue your quest? y/n")
+
+        if quest == "y": #Continue game
+            decision = True #Break the while decision loop
+            room = rooms()
+            baddie = baddies()
+        elif quest == "n": #End game
+            decision = True #To break the while decision loop
+            room = "quit"
+            baddie = "quit"
+            print("Goodbye!")
+        else:
+            print("Please enter either y or n.")
+            decision = False #Go through the while decision loop again
+
+    return room, baddie
+
+def death(health):
+    dead = True
+    if health <= 0:
+        dead = True
+        print("You are dead")
+    else:
+        dead = False
+    return dead
+
 def main():
     health = 100 #User's health
     attack = 10 #User's attack strength
-
+    first_time = True
     dead = False #If the user is dead, change to True
     while dead == False: #While the player is still alive
-        room = rooms() #Generating the room
-        print("You are in a " + room[0] + " " + room[1] + " room.")
-        enemy = baddies() #Generating random enemy from baddies()
-        enemy_name = enemy["name"]
-        enemy_health = int(enemy["health"])
-        enemy_attack = int(enemy["attack"])
+        room, enemy = next_step(first_time)
+        first_time = False
+        if room == "quit":
+            break
+        enemy_name = enemy["name"] #Populating enemy name
+        enemy_health = int(enemy["health"]) #Populating enemy health
+        enemy_attack = int(enemy["attack"]) #Populating enemy attack rating
+        print("You are in a " + room[0] + " " + room[1] + " room.\nYou're facing a: " + enemy_name + "\nEnemy health: " + str(enemy_health) + "\nEnemy attack rating: " + str(enemy_attack))#Description of room
         health = combat(health, attack, enemy_name, enemy_health, enemy_attack) #Begin combat. We only need health variable back.
-        if health <= 0: #Check for death
-            dead = True
-            print("You are dead.")
-        else:
-            decision = False #Decision to continue on quest
-            while decision == False and dead == False: #Continue loop until a decision is reached and also a check to see if the player is alive.
-                quest = input("Would you like to continue your quest? y/n")
-                if quest == "n": #End game
-                    dead = True #To break the while dead loop
-                    decision = True #To break the while decision loop
-                    print("Goodbye!")
-                elif quest == "y": #Continue game
-                    dead == False #Keep the while dead loop going
-                    decision = True #Break the while decision loop
-                else:
-                    print("Please enter either y or n.")
-                    decision = False #Go through the while decision loop again
+        dead = death(health)
+
+
 
 main()
